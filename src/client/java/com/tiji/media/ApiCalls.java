7fcfde30651e119ca6fa0a0e82008cdfbfc30983
Tiji <sjunesuh@gmail.com>
2025-03-10 18:05:42 +0800

    Api calls now include http methods

--- src/client/java/com/tiji/media/ApiCalls.java
+++ src/client/java/com/tiji/media/ApiCalls.java
@@ -13,18 +13,18 @@ import java.util.function.Consumer;
 
 public class ApiCalls {
     public static void convertAccessToken(String accessToken) {
-        call("accounts.spotify.com/api/token?grant_type=authorization_code&redirect_uri=http://localhost:25566/callback&code=" + accessToken,
+        call("https://accounts.spotify.com/api/token?grant_type=authorization_code&redirect_uri=http://localhost:25566/callback&code=" + accessToken,
                 getAuthorizationHeader(),
                 "application/x-www-form-urlencoded",
                 body -> {
                     JsonObject data = new Gson().fromJson(body.body(), JsonObject.class);
 
-                    if (!data.get("scope").getAsString().equals("user-read-playback-state user-modify-playback-state user-read-currently-playing")) return; // Authorization is modified
+                    if (!data.get("scope").getAsString().equals("user-modify-playback-state user-read-playback-state user-read-currently-playing")) return; // Authorization is modified
 
                     MediaClient.CONFIG.authToken(data.get("access_token").getAsString());
                     MediaClient.CONFIG.refreshToken(data.get("refresh_token").getAsString());
                     MediaClient.CONFIG.lastRefresh(System.currentTimeMillis());
-                });
+                }, "POST");
     }
     public static void refreshAccessToken() {
         call("https://accounts.spotify.com/api/token?grant_type=refresh_token&refresh_token=" + MediaClient.CONFIG.refreshToken(),
@@ -34,7 +34,7 @@ public class ApiCalls {
                     JsonObject data = new Gson().fromJson(body.body(), JsonObject.class);
 
                     if (data.has("error")) {
-                        Media.LOGGER.warn("Failed to refresh access token; Normally caused when developer app is deleted.");
+                        Media.LOGGER.warn("Failed to refresh access token; Normally caused when developer app is deleted. {}: {}", data.get("error"), data.get("error_description"));
                         MediaClient.CONFIG.reset();
                         try {
                             WebGuideServer.start();
@@ -47,20 +47,22 @@ public class ApiCalls {
                     MediaClient.CONFIG.authToken(data.get("access_token").getAsString());
                     if (data.has("refresh_token")) MediaClient.CONFIG.refreshToken(data.get("refresh_token").getAsString());
                     MediaClient.CONFIG.lastRefresh(System.currentTimeMillis());
-                });
+                }, "POST");
     }
     public static void getNowPlayingTrack(Consumer<JsonObject> callback) {
         call("https://api.spotify.com/v1/me/player",
                 getAuthorizationCode(),
                 null,
-                body -> callback.accept(new Gson().fromJson(body.body(), JsonObject.class))
+                body -> callback.accept(new Gson().fromJson(body.body(), JsonObject.class)),
+                "GET"
         );
     }
     public static void setPlaybackLoc(int position_ms) {
         call("https://api.spotify.com/v1/me/player/seek?position_ms=" + position_ms,
                 getAuthorizationCode(),
                 null,
-                body -> {}
+                body -> {},
+                "PUT"
         );
     }
     public static void playPause(boolean state) {
@@ -70,33 +72,47 @@ public class ApiCalls {
         }else {
             uri = "https://api.spotify.com/v1/me/player/pause";
         }
-        call(uri, getAuthorizationCode(), null, body -> {});
+        call(uri, getAuthorizationCode(), null, body -> {}, "PUT");
     }
     public static void nextTrack() {
-        call("https://api.spotify.com/v1/me/player/next", getAuthorizationCode(), null, body -> {});
+        call("https://api.spotify.com/v1/me/player/next", getAuthorizationCode(), null, body -> {}, "POST");
     }
     public static void previousTrack() {
-        call("https://api.spotify.com/v1/me/player/previous", getAuthorizationCode(), null, body -> {});
+        call("https://api.spotify.com/v1/me/player/previous", getAuthorizationCode(), null, body -> {}, "POST");
     }
     public static void getUserName(Consumer<String> consumer) {
         call("https://api.spotify.com/v1/me", getAuthorizationCode(), null, body -> {
             String name = new Gson().fromJson(body.body(), JsonObject.class).get("display_name").getAsString();
             consumer.accept(name);
-        });
+        }, "GET");
     }
-    private static void call(String endpoint, String Authorization, String ContentType, Consumer<HttpResponse<String>> consumer) {
+    private static void call(String endpoint, String Authorization, String ContentType, Consumer<HttpResponse<String>> consumer, String method) {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest.Builder request = HttpRequest.newBuilder()
-               .uri(URI.create(endpoint))
-               .timeout(Duration.ofSeconds(10))
-               .header("Authorization", Authorization);
+                .uri(URI.create(endpoint))
+                .timeout(Duration.ofSeconds(10))
+                .header("Authorization", Authorization);
         if (ContentType != null) request.header("Content-Type", ContentType);
 
+        request = switch (method) {
+            case "GET" -> request.GET();
+            case "POST" -> request.POST(HttpRequest.BodyPublishers.ofString(""));
+            case "PUT" -> request.PUT(HttpRequest.BodyPublishers.ofString(""));
+            default -> request;
+        };
+
         client.sendAsync(request.build(), HttpResponse.BodyHandlers.ofString())
-               .thenAccept(consumer)
                 .exceptionally(e -> {
-                    Media.LOGGER.error("Failed to make API call: {}", e.getMessage());
+                    Media.LOGGER.error("Failed to call API: {}", e.getMessage());
                     return null;
+                })
+                .thenAccept(stringHttpResponse -> {
+                    try{
+                        consumer.accept(stringHttpResponse);
+                    }catch (Exception e){
+                        Media.LOGGER.error("Failed to consume API response: ");
+                        e.printStackTrace();
+                    }
                 });
     }
     private static String getAuthorizationHeader() {
