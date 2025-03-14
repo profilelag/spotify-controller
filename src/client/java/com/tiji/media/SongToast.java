6627ed7b0b3becbeae8ee3358419a15771732fc1
Tiji <sjunesuh@gmail.com>
2025-03-14 23:17:26 +0800

    Update to 1.21.4

--- src/client/java/com/tiji/media/SongToast.java
+++ src/client/java/com/tiji/media/SongToast.java
@@ -1,6 +1,8 @@
 package com.tiji.media;
 
+import net.minecraft.client.font.TextRenderer;
 import net.minecraft.client.gui.DrawContext;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.toast.Toast;
 import net.minecraft.client.toast.ToastManager;
 import net.minecraft.util.Colors;
@@ -20,22 +22,30 @@ public class SongToast implements Toast {
         this.title = title;
     }
 
+    public void show(ToastManager manager) {
+        manager.add(this);
+    }
+
     @Override
-    public Visibility draw(DrawContext context, ToastManager manager, long startTime) {
-        if (this.startTime == null) {
-            this.startTime = System.currentTimeMillis();
-        }
-        context.drawTexture(TEXTURE, 0, 0, 1, 1, 160, 32, 160, 32);
-
-        context.drawText(manager.getClient().textRenderer, title, 35, 6, Colors.LIGHT_YELLOW, false);
-        context.drawText(manager.getClient().textRenderer, artist, 35, 18, Colors.WHITE, false);
-
-        context.drawTexture(cover, 0, 0, 0, 0, 32, 32, 32, 32);
-
+    public Visibility getVisibility() {
         return System.currentTimeMillis() - this.startTime > 3000 ? Visibility.HIDE : Visibility.SHOW;
     }
 
-    public void show(ToastManager manager) {
-        manager.add(this);
+    @Override
+    public void update(ToastManager manager, long time) {
+
+    }
+
+    @Override
+    public void draw(DrawContext context, TextRenderer textRenderer, long startTime) {
+        if (this.startTime == null) {
+            this.startTime = System.currentTimeMillis();
+        }
+        context.drawTexture(RenderLayer::getGuiTextured, TEXTURE, 0, 0, 1, 1, 160, 32, 160, 32);
+
+        context.drawText(textRenderer, title, 35, 6, Colors.LIGHT_YELLOW, false);
+        context.drawText(textRenderer, artist, 35, 18, Colors.WHITE, false);
+
+        context.drawTexture(RenderLayer::getGuiTextured, cover, 0, 0, 0, 0, 32, 32, 32, 32);
     }
 }
