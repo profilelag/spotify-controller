384b974dd2b9de07eb27bb4aac021a8ab2221bd7
Tiji <sjunesuh@gmail.com>
2025-03-14 23:18:08 +0800

    Progress widget is more visually appealing

--- src/client/java/com/tiji/media/widgets/progressWidget.java
+++ src/client/java/com/tiji/media/widgets/progressWidget.java
@@ -9,6 +9,9 @@ import io.github.cottonmc.cotton.gui.widget.data.InputResult;
 import net.minecraft.client.gui.DrawContext;
 
 public class progressWidget extends WSlider {
+    final int THUMB_SIZE = 4;
+    final int thumbHalf = THUMB_SIZE / 2;
+    final int trackHalf = TRACK_WIDTH / 2;
     public boolean allowUpdateProgress = true;
 
     public static final int TRACK_WIDTH = 2;
@@ -41,8 +44,13 @@ public class progressWidget extends WSlider {
     public void paint(DrawContext context, int x, int y, int mouseX, int mouseY) {
         int background = shouldRenderInDarkMode() ? 0xFF555555 : 0xFFAAAAAA;
         int foreground = shouldRenderInDarkMode() ? 0xFFFFFFFF : 0xFF333333;
-
+        final int thumbX = (int) (coordToValueRatio * (value - min)) + x;
         context.fill(x, y, width + x, y + TRACK_WIDTH, background);
-        context.fill(x, y, (int) (coordToValueRatio * (value - min)) + x, y + TRACK_WIDTH, foreground);
+        context.fill(x, y, thumbX, y + TRACK_WIDTH, foreground);
+        context.fill(thumbX - thumbHalf,
+                y - thumbHalf + trackHalf,
+                thumbX + thumbHalf,
+                y + thumbHalf + trackHalf,
+                foreground);
     }
 }
